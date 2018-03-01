b130126708ba5d7605048650949e01b2b2a77f33
Andrew971 <sordier.andrew@gmail.com>
2018-03-01 14:49:27 -0800

    update

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import Profile from './Profile'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+        userInfo:{}
+    }
+
+    this.apiUrl = ''
+
+  }
+
+  componentWillMount=()=>{
+    axios.get(this.apiUrl).then((res)=>{
+
+    })
+
+    
+
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
