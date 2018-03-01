69aa6940638cc49dd214add713c6fa77713d4292
illisan <illiv.sandra@gmail.com>
2018-03-01 14:48:58 -0800

    update

---
+++ src/Components/App/Nav/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+    render() {
+        return (
+            <div className="profile">
+
+
+            </div>
+        );
+    }
+}
+
+export default Nav;
--- src/Components/App/Profile/index.jsx
+++ src/Components/App/Profile/index.jsx
@@ -5,7 +5,18 @@ class Profile extends Component {
   render() {
     return (
       <div className="profile">
-      <img src =""/>
+        <img src="/profile_photo.jpeg" alt=""/>
+        <h3>Sandra</h3>
+        <ul>
+          <h3>Eats:</h3>
+          <li>Pizza</li>
+          <li>Sushi</li>
+          <li>Hotpot</li>
+          <li>Pho</li>
+          <li>Ramen</li>
+          <li>Tacos</li>
+        </ul>
+        <h4>Current Location: 455 Granville, Vancouver, Bc.</h4>
 
       </div>
     );
