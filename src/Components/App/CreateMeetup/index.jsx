4af69351a2a8c0f602e0dc3c0f51629b041d2158
Andrew971 <sordier.andrew@gmail.com>
2018-03-02 00:08:24 -0800

    ok

--- src/Components/App/CreateMeetup/index.jsx
+++ src/Components/App/CreateMeetup/index.jsx
@@ -14,7 +14,7 @@ export default class CreateMeetup extends Component {
       StartTime: '',
       EndTime: '',
       // CreatedBy:this.props.user.name
-      step:3
+      step:2
     }
     this.apiCreat = ''
   }
