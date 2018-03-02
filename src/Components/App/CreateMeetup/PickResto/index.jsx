c15fd917cc75b6ac66b33e0f0a0ac4e9287cb979
illisan <illiv.sandra@gmail.com>
2018-03-01 19:23:03 -0800

    list stlye updates

--- src/Components/App/CreateMeetup/PickResto/index.jsx
+++ src/Components/App/CreateMeetup/PickResto/index.jsx
@@ -2,24 +2,31 @@ import React, { Component } from 'react';
 
 
 class CreateMeetup extends Component {
-  constructor (){
+  constructor() {
     super()
-    this.state={
-      ListResto:[],
-      PickedResto:{},
-      StartTime:'',
-      EndTime:'',
-      CreatedBy:this.props.user.name
+    this.state = {
+      ListResto: [],
+      PickedResto: {},
+      StartTime: '',
+      EndTime: '',
+      CreatedBy: this.props.user.name
     }
   }
-  
-    render() {
-        return (
-            <div className="profile">
-              test
-            </div>
-        );
-    }
+
+  render() {
+    return (
+      <div className="restList">
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="noodles.jpeg" alt="" class="circle" />
+            <span class="title">Pholicious</span>
+            <p>First Line <br />Second Line</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default CreateMeetup;
--- src/Components/App/Profile/index.jsx
+++ src/Components/App/Profile/index.jsx
@@ -7,7 +7,7 @@ class Profile extends Component {
       <div className="profile col s12">
         <img className="circle responsive-img" src="/profile_photo.jpeg" alt="" />
         <h3 className="center-align">Sandra</h3>
-        <h4>Eats <i class="em em-hamburger"></i></h4>
+        <h4>Eats <i className="em em-hamburger"></i></h4>
         <ul className="bodyText">
           <li>Pizza</li>
           <li>Sushi</li>
@@ -17,7 +17,7 @@ class Profile extends Component {
           <li>Tacos</li>
         </ul>
         <h4>Current Location </h4>
-        <p className="bodyText"> <i class="em em-round_pushpin"></i>455 Granville, Vancouver, Bc.</p>
+        <p className="bodyText"> <i className="em em-round_pushpin"></i>455 Granville, Vancouver, Bc.</p>
         <h4>Bio</h4>
         <p className="bodyText">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis quam lorem. Praesent 
