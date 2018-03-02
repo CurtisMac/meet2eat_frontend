48db1a3516ee12ed91d6e827dae41ec2f0c4b5aa
Andrew971 <sordier.andrew@gmail.com>
2018-03-02 14:23:54 -0800

    ok

--- src/Components/App/CreateMeetup/PickResto/index.jsx
+++ src/Components/App/CreateMeetup/PickResto/index.jsx
@@ -15,7 +15,7 @@ export default class PickedResto extends Component {
             <p>{restaurant.location.adress}<br />{restaurant.user_rating.rating_text}</p>
             <a href={restaurant.menu_url} className="secondary-content"><i className="material-icons">restaurant_menu</i></a>
             {/* <a className="btn btn-large" type="button" onClick={()=>{ this.props.pickedResto(2)}} >pick</a> */}
-           <a className="btn-floating btn-small red" onClick={() => { this.props.pickedResto(2) }} ><i className="material-icons">add</i></a>
+           <a className="btn-floating btn-small red" onClick={() => { this.props.pickedResto(restaurant) }} ><i className="material-icons">add</i></a>
           </li>
         // <li key={restaurant.id} target="_self">
         //   <img src={restaurant.featured_image} alt="" />
--- src/Components/App/JoinMeetup/PickedTime/index.jsx
+++ src/Components/App/JoinMeetup/PickedTime/index.jsx
@@ -11,7 +11,9 @@ export default class PickedTime extends Component {
         <h3 className="center-align">Pick a time</h3>
         <h3 className="center-align"><i className="large material-icons">access_time</i></h3>
         <div className="row">
-          <form className="col s12">
+          <form className="col s12" ref={self => this.TimeForm = self} onSubmit={(e) => { 
+            e.preventDefault
+            this.props.PickedTime(this.TimeForm) }}>
             <div className="row">
               <div className="input-field col s12">
                 <input id="timer" type="text" />
