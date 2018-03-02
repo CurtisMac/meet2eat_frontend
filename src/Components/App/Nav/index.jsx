640ef00b4b2e14ddf61a5966644449aef961f643
illisan <illiv.sandra@gmail.com>
2018-03-02 03:16:06 -0800

    nav icon update

--- src/Components/App/Nav/index.jsx
+++ src/Components/App/Nav/index.jsx
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router-dom'
 
 class Nav extends Component {
     render() {
-        return (
+        return ( 
             <nav>
                 <div className="nav-wrapper">
                     <ul className="iconNav">
                         <li><NavLink to="/"><i className="material-icons">face</i></NavLink></li>
-                        <li><NavLink to="/join"><i className="fas fa-utensils fa-2x"></i></NavLink></li>
+                        <li><NavLink to="/create"><i className="material-icons">group</i></NavLink></li>
                         <li><NavLink to="/create"><i className="material-icons">add</i></NavLink></li>
                     </ul>
                 </div>
