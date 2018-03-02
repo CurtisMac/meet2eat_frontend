31db5eedc1181b9ce28b936e84984d6e9b034ab8
illisan <illiv.sandra@gmail.com>
2018-03-02 14:23:11 -0800

    ok

--- src/Components/App/CreateMeetup/PickResto/index.jsx
+++ src/Components/App/CreateMeetup/PickResto/index.jsx
@@ -10,21 +10,11 @@ export default class PickedResto extends Component {
       let restaurant= resto.restaurant
       return(
         <li className="collection-item avatar" key={restaurant.id} target="_self">
-            <img src={restaurant.featured_image} alt="" className="circle" />
+          <img src={restaurant.featured_image} alt="" className="circle" onClick={() => { this.props.pickedResto(2) }}/>
             <span className="title">{restaurant.name} </span>
             <p>{restaurant.location.adress}<br />{restaurant.user_rating.rating_text}</p>
             <a href={restaurant.menu_url} className="secondary-content"><i className="material-icons">restaurant_menu</i></a>
-            {/* <a className="btn btn-large" type="button" onClick={()=>{ this.props.pickedResto(2)}} >pick</a> */}
-           <a className="btn-floating btn-small red" onClick={() => { this.props.pickedResto(2) }} ><i className="material-icons">add</i></a>
           </li>
-        // <li key={restaurant.id} target="_self">
-        //   <img src={restaurant.featured_image} alt="" />
-        //   <span className="title">{restaurant.name} </span>
-        //   <p>{restaurant.location.adress}<br />{restaurant.user_rating.rating_text}</p>
-        //   <a href={restaurant.menu_url} className="secondary-content"><i className="material-icons">restaurant_menu</i></a>
-        //   {/* <a className="btn btn-large" type="button" onClick={()=>{ this.props.pickedResto(2)}} >pick</a> */}
-        //   <button type="button" onClick={() => { this.props.pickedResto(2) }} ><i className="material-icons">add</i></button>
-        // </li>
       )
     })
     return (
