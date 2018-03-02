d6f1c83a82eb7c8549b27ed4da225aaf10411574
Andrew971 <sordier.andrew@gmail.com>
2018-03-01 21:08:04 -0800

    ok

--- src/Components/App/CreateMeetup/complete.jsx
+++ src/Components/App/CreateMeetup/complete.jsx
@@ -8,11 +8,7 @@ export default class Complete extends Component {
   render() {
     return (
       <div className="restList">
-<<<<<<< HEAD
-      test
-=======
       <h2>test test</h2>
->>>>>>> dfe15914418ad66da462849a4f2dd751b6a8338f
       </div>
     );
   }
