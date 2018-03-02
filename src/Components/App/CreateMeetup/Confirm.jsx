3bae42d546a8c6d8dd863f67fde6ce35000bd5a7
illisan <illiv.sandra@gmail.com>
2018-03-01 22:48:35 -0800

    confirm screen update

--- src/Components/App/CreateMeetup/Confirm.jsx
+++ src/Components/App/CreateMeetup/Confirm.jsx
@@ -8,8 +8,8 @@ export default class Confirm extends Component {
     render() {
         return (
             <div className="confrimMeet">
-                <h3>Almost there!</h3>
-                <h4>Confirm the details below:</h4>
+                <h3 className="center-align">Almost there!</h3>
+                <h4 className="center-align">Confirm the details below:</h4>
                 <ul className="collection">
                     <li className="collection-item avatar">
                         <img src="/noodles.jpeg" alt="" className="circle" />
@@ -21,8 +21,8 @@ export default class Confirm extends Component {
                     </li>
                 </ul>
                 <div className="buttons">
-                    <a class="btn">back</a>
-                    <a class="btn">confirm</a>
+                    <a className="btn btn-large">back</a>
+                    <a className="btn btn-large">confirm</a>
                 </div>
             </div>
         );
