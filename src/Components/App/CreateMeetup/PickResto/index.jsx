72bb009d0d266321cd54d89c79621d602bc7bef8
illisan <illiv.sandra@gmail.com>
2018-03-01 19:34:36 -0800

    lol

--- src/Components/App/CreateMeetup/PickResto/index.jsx
+++ src/Components/App/CreateMeetup/PickResto/index.jsx
@@ -16,12 +16,12 @@ export default class PickedResto extends Component {
   render() {
     return (
       <div className="restList">
-        <ul class="collection">
-          <li class="collection-item avatar">
+        <ul className="collection">
+          <li className="collection-item avatar">
             <img src="noodles.jpeg" alt="" class="circle" />
-            <span class="title">Pholicious</span>
-            <p>First Line <br />Second Line</p>
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            <span className="title">Pholicious</span>
+            <p><br />Second Line</p>
+            <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
           </li>
         </ul>
       </div>
